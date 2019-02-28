992a4ba060492a5e69a7d0ef9814319b087bfb5b
Floyd 2001 <floyd2001@floyds-mbp>
2019-02-28 10:14:17 -0800

    Initial Commit

---
+++ collectionViewLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionViewLayout
+//
+//  Created by Floyd 2001 on 2/28/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
